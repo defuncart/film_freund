6e801619e17c83dab6a23da96f72c38d721f2881
James Leahy <defuncart@gmail.com>
2021-08-12 13:50:20 +0200

    Add POC mobile/non-mobile HomeScreen.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:film_freund/generated/l10n.dart';
+import 'package:film_freund/utils/sizes.dart';
 import 'package:film_freund/widgets/home_screen/sidebar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +10,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(AppLocalizations.of(context).test),
-      ),
-      // TODO only needed for web/desktop
-      drawer: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.7,
-        child: const Sidebar(),
-      ),
-      drawerEnableOpenDragGesture: false,
-    );
+    return isMobile(context)
+        ? Scaffold(
+            appBar: AppBar(),
+            body: Center(
+              child: Text(AppLocalizations.of(context).test),
+            ),
+            // TODO only needed for web/desktop
+            drawer: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: const Sidebar(),
+            ),
+            drawerEnableOpenDragGesture: false,
+          )
+        : Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: 200,
+                      maxWidth: min(max(200, MediaQuery.of(context).size.width * 0.25), 300),
+                    ),
+                    child: const Sidebar(),
+                  ),
+                  const VerticalDivider(width: 2),
+                  Expanded(
+                    child: Center(
+                      child: Text(AppLocalizations.of(context).test),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
   }
 }
