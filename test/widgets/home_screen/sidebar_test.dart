f521d7c063b563369d3639b4feb82a8587a37d81
James Leahy <defuncart@gmail.com>
2021-08-13 10:52:48 +0200

    Add more tests for sidebar.

--- test/widgets/home_screen/sidebar_test.dart
+++ test/widgets/home_screen/sidebar_test.dart
@@ -1,3 +1,4 @@
+import 'package:film_freund/generated/l10n.dart';
 import 'package:film_freund/widgets/home_screen/active_view.dart';
 import 'package:film_freund/widgets/home_screen/sidebar.dart';
 import 'package:flutter/material.dart';
@@ -28,5 +29,49 @@ void main() {
     for (final element in elements) {
       expect(find.text(element.view.title), findsOneWidget);
     }
+  });
+
+  testWidgets('Ensure onViewChanged can be invoked', (tester) async {
+    var activeView = ActiveView.popular;
+    final widget = wrapWithMaterialAppLocalizationDelegates(
+      Sidebar(
+        onViewChanged: (newView) => activeView = newView,
+        onSignOut: () {},
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+
+    await tester.pumpAndSettle();
+
+    expect(activeView, ActiveView.popular);
+
+    await tester.tap(
+      find.text(AppLocalizations.current.activeViewSearchTitle),
+    );
+
+    expect(activeView, ActiveView.search);
+  });
+
+  testWidgets('Ensure onSignOut can be invoked', (tester) async {
+    var isPressed = false;
+    final widget = wrapWithMaterialAppLocalizationDelegates(
+      Sidebar(
+        onViewChanged: (_) {},
+        onSignOut: () => isPressed = true,
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+
+    await tester.pumpAndSettle();
+
+    expect(isPressed, isFalse);
+
+    await tester.tap(
+      find.text(AppLocalizations.current.sidebarSignOutButtonText),
+    );
+
+    expect(isPressed, isTrue);
   });
 }
