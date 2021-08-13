bc69b14c1594473c5af685875f96b9e75d2b3d9b
James Leahy <defuncart@gmail.com>
2021-08-13 10:24:35 +0200

    Add SignOutConfirmationDialog.

---
+++ lib/widgets/home_screen/sign_out_confirmation_dialog.dart
@@ -0,0 +1,35 @@
+import 'package:film_freund/generated/l10n.dart';
+import 'package:flutter/material.dart';
+
+class SignOutConfirmationDialog extends StatelessWidget {
+  const SignOutConfirmationDialog({
+    required this.onConfirm,
+    Key? key,
+  }) : super(key: key);
+
+  final VoidCallback onConfirm;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Text(AppLocalizations.of(context).signOutConfirmationDialogDescriptionText),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text(
+            AppLocalizations.of(context).signOutConfirmationDialogCancelButtonText.toUpperCase(),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+            onConfirm();
+          },
+          child: Text(
+            AppLocalizations.of(context).signOutConfirmationDialogConfirmButtonText.toUpperCase(),
+          ),
+        ),
+      ],
+    );
+  }
+}
