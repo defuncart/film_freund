11d1c93387b0e18763da41fb791d0d1975dd2319
James Leahy <defuncart@gmail.com>
2021-08-21 12:24:50 +0200

    Add SignInErrorDialog.

---
+++ test/widgets/signin_screen/signin_error_dialog_test.dart
@@ -0,0 +1,37 @@
+import 'package:film_freund/generated/l10n.dart';
+import 'package:film_freund/widgets/signin_screen/signin_error_dialog.dart';
+import 'package:flutter/material.dart' show Text;
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../test_utils.dart';
+
+void main() {
+  group('$SignInErrorDialog', () {
+    final widget = wrapWithMaterialAppLocalizationDelegates(
+      const SignInErrorDialog(),
+    );
+
+    testWidgets('Ensure content is correct', (tester) async {
+      await tester.pumpWidget(widget);
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(Text), findsNWidgets(3));
+
+      expect(
+        find.text(AppLocalizations.current.signinErrorDialogTitleText),
+        findsOneWidget,
+      );
+
+      expect(
+        find.text(AppLocalizations.current.signinErrorDialogDescriptionText),
+        findsOneWidget,
+      );
+
+      expect(
+        find.text(AppLocalizations.current.signinErrorDialogButtonText.toUpperCase()),
+        findsOneWidget,
+      );
+    });
+  });
+}
