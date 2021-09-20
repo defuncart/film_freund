542031e88306f63df774341aed3477eb7ae0fafd
James Leahy <defuncart@gmail.com>
2021-09-20 09:17:28 +0100

    User: equatable over freezed, add methods to override instance.

--- lib/services/user/models/user.dart
+++ lib/services/user/models/user.dart
@@ -1,21 +1,81 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
 
-part 'user.freezed.dart';
 part 'user.g.dart';
 
-@freezed
-class User with _$User {
-  factory User({
-    required String id,
-    required String email,
-    @Default('') String firstName,
-    @Default('') String lastName,
-    required DateTime createdAt,
-    required DateTime updatedAt,
-    @Default([]) List<String> watched,
-    @Default([]) List<String> watchlist,
-    @Default([]) List<String> lists,
-  }) = _User;
+@JsonSerializable()
+@immutable
+class User extends Equatable {
+  const User({
+    required this.id,
+    required this.email,
+    this.firstName = '',
+    this.lastName = '',
+    required this.createdAt,
+    required this.updatedAt,
+    this.watched = const [],
+    this.watchlist = const [],
+    this.lists = const [],
+  });
+
+  final String id;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final List<String> watched;
+  final List<String> watchlist;
+  final List<String> lists;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  /// Returns a copy of current instance with overriden params
+  User copyWith({
+    String? firstName,
+    String? lastName,
+    List<String>? watched,
+    List<String>? watchlist,
+    List<String>? lists,
+  }) =>
+      User(
+        id: id,
+        email: email,
+        firstName: firstName ?? this.firstName,
+        lastName: lastName ?? this.lastName,
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        watched: watched ?? this.watched,
+        watchlist: watchlist ?? this.watchlist,
+        lists: lists ?? this.lists,
+      );
+
+  /// Returns a copy of current instance with overriden [updatedAt]
+  User setUpdatedAt(DateTime updatedAt) => User(
+        id: id,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        createdAt: createdAt,
+        updatedAt: updatedAt,
+        watched: watched,
+        watchlist: watchlist,
+        lists: lists,
+      );
+
+  @override
+  List<Object?> get props => [
+        id,
+        email,
+        firstName,
+        lastName,
+        createdAt,
+        updatedAt,
+        watched,
+        watchlist,
+        lists,
+      ];
 }
--- lib/services/user/models/user.g.dart
+++ lib/services/user/models/user.g.dart
@@ -6,28 +6,23 @@ part of 'user.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_User _$_$_UserFromJson(Map<String, dynamic> json) {
-  return _$_User(
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User(
     id: json['id'] as String,
     email: json['email'] as String,
-    firstName: json['firstName'] as String? ?? '',
-    lastName: json['lastName'] as String? ?? '',
+    firstName: json['firstName'] as String,
+    lastName: json['lastName'] as String,
     createdAt: DateTime.parse(json['createdAt'] as String),
     updatedAt: DateTime.parse(json['updatedAt'] as String),
     watched:
-        (json['watched'] as List<dynamic>?)?.map((e) => e as String).toList() ??
-            [],
-    watchlist: (json['watchlist'] as List<dynamic>?)
-            ?.map((e) => e as String)
-            .toList() ??
-        [],
-    lists:
-        (json['lists'] as List<dynamic>?)?.map((e) => e as String).toList() ??
-            [],
+        (json['watched'] as List<dynamic>).map((e) => e as String).toList(),
+    watchlist:
+        (json['watchlist'] as List<dynamic>).map((e) => e as String).toList(),
+    lists: (json['lists'] as List<dynamic>).map((e) => e as String).toList(),
   );
 }
 
-Map<String, dynamic> _$_$_UserToJson(_$_User instance) => <String, dynamic>{
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
       'firstName': instance.firstName,
