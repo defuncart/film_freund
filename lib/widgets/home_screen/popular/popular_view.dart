815d4352e4d035100b0a4cfc63a3e02bd3e58136
James Leahy <defuncart@gmail.com>
2021-09-23 08:41:50 +0100

    Add PopularView.

---
+++ lib/widgets/home_screen/popular/popular_view.dart
@@ -0,0 +1,26 @@
+import 'package:film_freund/services/movies/models/movie_teaser.dart';
+import 'package:film_freund/services/service_locator.dart';
+import 'package:film_freund/widgets/common/movie/movie_teasers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PopularView extends ConsumerWidget {
+  const PopularView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(popularMoviesProvider);
+
+    return movies.when(
+      loading: () => const CircularProgressIndicator(),
+      error: (err, stack) => Text('Error: $err'),
+      data: (movies) => MovieTeasers(
+        movieTeasers: movies,
+      ),
+    );
+  }
+}
+
+final popularMoviesProvider = FutureProvider.autoDispose<List<MovieTeaser>>((ref) async {
+  return ServiceLocator.movieDatabase.getPopular();
+});
