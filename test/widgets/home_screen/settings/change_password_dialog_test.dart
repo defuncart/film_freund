85917cb4f4b3eb955398b2a766aaadff63b1c341
James Leahy <defuncart@gmail.com>
2021-09-25 18:58:51 +0100

    ChangePasswordDialog add more tests.

--- test/widgets/home_screen/settings/change_password_dialog_test.dart
+++ test/widgets/home_screen/settings/change_password_dialog_test.dart
@@ -1,9 +1,15 @@
 import 'package:film_freund/generated/l10n.dart';
+import 'package:film_freund/managers/user/user_manager.dart';
+import 'package:film_freund/services/auth/i_auth_service.dart';
 import 'package:film_freund/widgets/common/input_fields/password_input.dart';
 import 'package:film_freund/widgets/home_screen/settings/change_password_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_ui/flutter_test_ui.dart';
+import 'package:mockito/mockito.dart';
 
+import '../../../mocks.dart';
+import '../../../test_service_locator.dart';
 import '../../../test_utils.dart';
 
 void main() {
@@ -105,7 +111,7 @@ void main() {
         );
       });
 
-      testWidgets('when same as current, expect no error', (tester) async {
+      testWidgets('when same as current, expect error', (tester) async {
         await tester.pumpWidget(widget);
         await tester.pumpAndSettle();
 
@@ -119,9 +125,27 @@ void main() {
 
         expect(
           find.text(AppLocalizations.current.changePasswordDialogNewPasswordErrorText),
+          findsOneWidget,
+        );
+      });
+
+      testWidgets('when not same as current, expect no error', (tester) async {
+        await tester.pumpWidget(widget);
+        await tester.pumpAndSettle();
+
+        await tester.enterText(find.byKey(ChangePasswordDialogKeys.currentPasswordTextField), '123456');
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pump();
+
+        await tester.enterText(find.byKey(ChangePasswordDialogKeys.newPasswordTextField), '654321');
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pump();
+
+        expect(
+          find.text(AppLocalizations.current.changePasswordDialogNewPasswordErrorText),
           findsNothing,
         );
-      }, skip: true);
+      });
     });
 
     group('repeat password', () {
@@ -181,6 +205,114 @@ void main() {
         tester.widget<TextButton>(textButtonFinder).enabled,
         isFalse,
       );
+    });
+
+    group('when current, new and repeat passwords are  valid', () {
+      const currentPassword = '123456';
+      const newPassword = '654321';
+      final UserManager mockUserManager = MockUserManager();
+      TestServiceLocator.register(
+        userManager: mockUserManager,
+      );
+
+      setUpUI((tester) async {
+        await tester.pumpWidget(widget);
+        await tester.pumpAndSettle();
+
+        await tester.enterText(find.byKey(ChangePasswordDialogKeys.currentPasswordTextField), currentPassword);
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.enterText(find.byKey(ChangePasswordDialogKeys.newPasswordTextField), newPassword);
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.enterText(find.byKey(ChangePasswordDialogKeys.repeatNewPasswordTextField), newPassword);
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pump();
+      });
+
+      testUI('expect no error', (tester) async {
+        expect(
+          find.text(AppLocalizations.current.generalInvalidPassword),
+          findsNothing,
+        );
+        expect(
+          find.text(AppLocalizations.current.changePasswordDialogNewPasswordErrorText),
+          findsNothing,
+        );
+        expect(
+          find.text(AppLocalizations.current.changePasswordDialogRepeatNewPasswordErrorText),
+          findsNothing,
+        );
+      });
+
+      testUI('expect continue button is enabled', (tester) async {
+        final textButtonFinder = find.ancestor(
+          of: find.text(
+            AppLocalizations.current.changePasswordDialogConfirmButtonText.toUpperCase(),
+          ),
+          matching: find.byType(TextButton),
+        );
+
+        expect(
+          tester.widget<TextButton>(textButtonFinder).enabled,
+          isTrue,
+        );
+      });
+
+      group('when cancel button is pressed', () {
+        testUI('ensure no callbacks are invoked', (tester) async {
+          await tester.tap(find.text(
+            AppLocalizations.current.generalCancel.toUpperCase(),
+          ));
+
+          verifyNever(mockUserManager.changePassword(
+            currentPassword: currentPassword,
+            newPassword: newPassword,
+          ));
+        });
+      });
+
+      group('when continue button is pressed', () {
+        testUI('and ${ChangePasswordResult.success}, ensure callback is invoked', (tester) async {
+          when(mockUserManager.changePassword(currentPassword: currentPassword, newPassword: newPassword))
+              .thenAnswer((_) => Future.value(ChangePasswordResult.success));
+
+          await tester.tap(find.text(
+            AppLocalizations.current.changePasswordDialogConfirmButtonText.toUpperCase(),
+          ));
+          await tester.pumpAndSettle();
+
+          expect(find.byType(ChangePasswordDialog), findsNothing);
+        });
+
+        testUI('and ${ChangePasswordResult.incorrectPassword}, ensure incorrect password error', (tester) async {
+          when(mockUserManager.changePassword(currentPassword: currentPassword, newPassword: newPassword))
+              .thenAnswer((_) => Future.value(ChangePasswordResult.incorrectPassword));
+
+          await tester.tap(find.text(
+            AppLocalizations.current.changePasswordDialogConfirmButtonText.toUpperCase(),
+          ));
+          await tester.pumpAndSettle();
+
+          expect(
+            find.text(AppLocalizations.current.generalIncorrectPassword),
+            findsOneWidget,
+          );
+        });
+
+        testUI('and ${ChangePasswordResult.other}, ensure general error', (tester) async {
+          when(mockUserManager.changePassword(currentPassword: currentPassword, newPassword: newPassword))
+              .thenAnswer((_) => Future.value(ChangePasswordResult.other));
+
+          await tester.tap(find.text(
+            AppLocalizations.current.changePasswordDialogConfirmButtonText.toUpperCase(),
+          ));
+          await tester.pumpAndSettle();
+
+          expect(
+            find.text(AppLocalizations.current.generalErrorOccured),
+            findsOneWidget,
+          );
+        });
+      });
     });
   });
 }
