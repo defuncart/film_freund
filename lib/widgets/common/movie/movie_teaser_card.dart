f5086faa7f22e60453a6d4d59d779d1204b03547
James Leahy <defuncart@gmail.com>
2021-09-27 11:26:02 +0100

    MovieTeasers reactive set card's size.

--- lib/widgets/common/movie/movie_teaser_card.dart
+++ lib/widgets/common/movie/movie_teaser_card.dart
@@ -4,13 +4,19 @@ import 'package:film_freund/services/movies/models/movie_teaser.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+/// A card to tease a movie
+///
+/// [width] must be greater than 0. Height is automatically calculated as 1.5 * [width]
 class MovieTeaserCard extends StatelessWidget {
   const MovieTeaserCard({
     required this.movieTeaser,
+    required this.width,
     Key? key,
-  }) : super(key: key);
+  })  : assert(width > 0),
+        super(key: key);
 
   final MovieTeaser movieTeaser;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +40,8 @@ class MovieTeaserCard extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: const BorderRadius.all(Radius.circular(8)),
                   child: SizedBox(
-                    width: 150,
-                    height: 225,
+                    width: width,
+                    height: width * 1.5,
                     child: Image.network(
                       // TODO account for null case
                       movieTeaser.posterPath!,
--- lib/widgets/common/movie/movie_teasers.dart
+++ lib/widgets/common/movie/movie_teasers.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:film_freund/services/movies/models/movie_teaser.dart';
 import 'package:film_freund/widgets/common/movie/movie_teaser_card.dart';
 import 'package:flutter/material.dart';
@@ -12,19 +14,27 @@ class MovieTeasers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Wrap(
-          spacing: 16,
-          runSpacing: 16,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final width = max(100.0, min(200.0, constraints.maxWidth / 4));
+
+        return ListView(
           children: [
-            for (final movieTeaser in movieTeasers)
-              MovieTeaserCard(
-                movieTeaser: movieTeaser,
-              ),
+            Wrap(
+              spacing: 16,
+              runSpacing: 16,
+              alignment: WrapAlignment.spaceEvenly,
+              children: [
+                for (final movieTeaser in movieTeasers)
+                  MovieTeaserCard(
+                    movieTeaser: movieTeaser,
+                    width: width,
+                  ),
+              ],
+            ),
           ],
-        ),
-      ],
+        );
+      },
     );
   }
 }
