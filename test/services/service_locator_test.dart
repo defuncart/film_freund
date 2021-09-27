58b2112155950f3dbe3a32b233b9895905f5230b
James Leahy <defuncart@gmail.com>
2021-09-27 10:40:05 +0100

    ServiceLocator improve coverage.

--- test/services/service_locator_test.dart
+++ test/services/service_locator_test.dart
@@ -1,28 +1,69 @@
+import 'package:film_freund/managers/user/user_manager.dart';
 import 'package:film_freund/services/date_time/date_time_service.dart';
+import 'package:film_freund/services/movies/i_movie_database.dart';
 import 'package:film_freund/services/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../firebase_auth_mock.dart';
 import '../mocks.dart';
 import '../test_service_locator.dart';
 
 void main() {
   group('$ServiceLocator', () {
-    late DateTimeService mockDateTimeService;
+    test('When $ServiceLocator is initialized, expect access to services', () async {
+      setupFirebaseAuthMocks();
+      await Firebase.initializeApp();
+      final container = ProviderContainer();
+      ServiceLocator.initialize(container.read);
 
-    setUp(() {
-      mockDateTimeService = MockDateTimeService();
-      TestServiceLocator.register(
-        dateTimeService: mockDateTimeService,
-      );
-    });
-
-    tearDown(TestServiceLocator.reset);
-
-    test('When $ServiceLocator is initialized, expect access to services', () {
       expect(
         () => ServiceLocator.dateTimeService,
         returnsNormally,
       );
+      expect(
+        () => ServiceLocator.userManager,
+        returnsNormally,
+      );
+      expect(
+        () => ServiceLocator.movieDatabase,
+        returnsNormally,
+      );
+    });
+
+    group('ensure services can be mocked', () {
+      late DateTimeService mockDateTimeService;
+      late UserManager mockUserManager;
+      late IMovieDatabase mockMovieDatabase;
+
+      setUp(() {
+        mockDateTimeService = MockDateTimeService();
+        mockUserManager = MockUserManager();
+        mockMovieDatabase = MockIMovieDatabase();
+        TestServiceLocator.register(
+          dateTimeService: mockDateTimeService,
+          userManager: mockUserManager,
+          movieDatabase: mockMovieDatabase,
+        );
+      });
+
+      tearDown(TestServiceLocator.reset);
+
+      test('When $ServiceLocator is initialized, expect access to services', () {
+        expect(
+          () => ServiceLocator.dateTimeService,
+          returnsNormally,
+        );
+        expect(
+          () => ServiceLocator.userManager,
+          returnsNormally,
+        );
+        expect(
+          () => ServiceLocator.movieDatabase,
+          returnsNormally,
+        );
+      });
     });
   });
 }
