64a1d0834901412948fba8adae832ca232c78dc3
James Leahy <defuncart@gmail.com>
2021-09-29 09:16:10 +0100

    Add MovieTeasersContainer.

---
+++ lib/widgets/common/movie/movie_teasers_container.dart
@@ -0,0 +1,27 @@
+import 'package:film_freund/services/movies/models/movie_teaser.dart';
+import 'package:film_freund/widgets/common/movie/movie_teasers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// A [ConsumerWidget] which takes a [provider] of `List<MovieTeasers>` and displays appropriate state
+class MovieTeasersContainer extends ConsumerWidget {
+  const MovieTeasersContainer({
+    required this.provider,
+    Key? key,
+  }) : super(key: key);
+
+  final AutoDisposeFutureProvider<List<MovieTeaser>> provider;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final movies = ref.watch(provider);
+
+    return movies.when(
+      loading: () => const CircularProgressIndicator(),
+      error: (err, stack) => Text(err.toString()),
+      data: (movies) => MovieTeasers(
+        movieTeasers: movies,
+      ),
+    );
+  }
+}
