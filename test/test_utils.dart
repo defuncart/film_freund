c9e903de63c8d52c374b8fbfe8afe1ecd5f627de
James Leahy <defuncart@gmail.com>
2021-09-29 08:38:02 +0100

    Add GoldenUtils.

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:film_freund/generated/l10n.dart';
 import 'package:film_freund/services/movies/models/movie.dart';
 import 'package:film_freund/services/movies/models/movie_teaser.dart';
@@ -116,4 +118,17 @@ class TestInstance {
         voteAverage: voteAverage ?? 0,
         voteCount: voteCount ?? 0,
       );
+}
+
+/// Utils for golden tests
+abstract class GoldenUtils {
+  /// A tag to mark a test as a golden test
+  static const tag = 'goldens';
+
+  /// Generates a filepath for a golden for a [testFilepath] and [imageName]
+  static String generateFilepath({
+    required String testFilepath,
+    required String imageName,
+  }) =>
+      '${Directory.current.path}/test/goldens/$testFilepath/$imageName.png';
 }
