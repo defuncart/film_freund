5a633e05c92c11338704a5d5e2832c0ecd71a9fe
James Leahy <defuncart@gmail.com>
2021-10-17 12:56:08 +0200

    Ensure MovieWatchStatus is comparable.

--- lib/state/movie_watch_status_provider.dart
+++ lib/state/movie_watch_status_provider.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:film_freund/services/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,7 +24,7 @@ final isMovieWatchlistProvider = StreamProvider.family<bool, int>((ref, id) {
   return ServiceLocator.movieManager.watchWatchlist.map((list) => list.movies.contains(id));
 });
 
-class MovieWatchStatus {
+class MovieWatchStatus extends Equatable {
   const MovieWatchStatus({
     required this.isWatched,
     required this.isWatchlist,
@@ -31,4 +32,10 @@ class MovieWatchStatus {
 
   final bool isWatched;
   final bool isWatchlist;
+
+  @override
+  List<Object?> get props => [
+        isWatched,
+        isWatchlist,
+      ];
 }
