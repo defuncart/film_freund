5a316b12ec6b2c40558335eca7f81a3862a6d947
James Leahy <defuncart@gmail.com>
2022-01-09 16:10:57 +0100

    Update generated code

--- lib/services/user/models/user.g.dart
+++ lib/services/user/models/user.g.dart
@@ -6,18 +6,18 @@ part of 'user.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-User _$UserFromJson(Map<String, dynamic> json) {
-  return User(
-    id: json['id'] as String,
-    email: json['email'] as String,
-    displayName: json['displayName'] as String,
-    createdAt: DateTime.parse(json['createdAt'] as String),
-    updatedAt: DateTime.parse(json['updatedAt'] as String),
-    watchedId: json['watchedId'] as String,
-    watchlistId: json['watchlistId'] as String,
-    lists: (json['lists'] as List<dynamic>).map((e) => e as String).toList(),
-  );
-}
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      displayName: json['displayName'] as String? ?? '',
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+      watchedId: json['watchedId'] as String,
+      watchlistId: json['watchlistId'] as String,
+      lists:
+          (json['lists'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+              const [],
+    );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'id': instance.id,
