538c54a56a73dfc5bfba717327931a0591bfb368
James Leahy <defuncart@gmail.com>
2022-01-22 11:01:53 +0100

    MovieTeasersAsyncValue add optional noData param

--- test/widgets/common/movies/movie_teasers_async_value_test.dart
+++ test/widgets/common/movies/movie_teasers_async_value_test.dart
@@ -22,19 +22,67 @@ void main() {
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
     });
 
-    testWidgets('when value has data, expect $MovieTeasers', (tester) async {
-      mockNetworkImagesFor(() async {
-        await tester.pumpWidget(
-          ProviderScope(
-            child: wrapWithMaterialApp(
-              MovieTeasersAsyncValue(AsyncValue.data([TestInstance.movieTeaser()])),
-            ),
-          ),
-        );
-        await tester.pumpAndSettle();
+    group('when value has data', () {
+      const noData = 'noData';
 
-        expect(find.byType(MovieTeasersAsyncValue), findsOneWidget);
-        expect(find.byType(MovieTeasers), findsOneWidget);
+      testWidgets('and data is not empty, expect $MovieTeasers', (tester) async {
+        mockNetworkImagesFor(() async {
+          await tester.pumpWidget(
+            ProviderScope(
+              child: wrapWithMaterialApp(
+                MovieTeasersAsyncValue(
+                  AsyncValue.data([TestInstance.movieTeaser()]),
+                  noData: const Text(noData),
+                ),
+              ),
+            ),
+          );
+          await tester.pumpAndSettle();
+
+          expect(find.byType(MovieTeasersAsyncValue), findsOneWidget);
+          expect(find.byType(MovieTeasers), findsOneWidget);
+          expect(find.byType(Text), findsNothing);
+          expect(find.text(noData), findsNothing);
+        });
+      });
+
+      group('and data is empty', () {
+        testWidgets('and noData is given, expect noData', (tester) async {
+          await tester.pumpWidget(
+            ProviderScope(
+              child: wrapWithMaterialApp(
+                const MovieTeasersAsyncValue(
+                  AsyncValue.data([]),
+                  noData: Text(noData),
+                ),
+              ),
+            ),
+          );
+          await tester.pumpAndSettle();
+
+          expect(find.byType(MovieTeasersAsyncValue), findsOneWidget);
+          expect(find.byType(MovieTeasers), findsNothing);
+          expect(find.byType(Text), findsOneWidget);
+          expect(find.text(noData), findsOneWidget);
+        });
+
+        testWidgets('and noData is not given, expect nothing', (tester) async {
+          await tester.pumpWidget(
+            ProviderScope(
+              child: wrapWithMaterialApp(
+                const MovieTeasersAsyncValue(
+                  AsyncValue.data([]),
+                ),
+              ),
+            ),
+          );
+          await tester.pumpAndSettle();
+
+          expect(find.byType(MovieTeasersAsyncValue), findsOneWidget);
+          expect(find.byType(MovieTeasers), findsNothing);
+          expect(find.byType(Text), findsNothing);
+          expect(find.text(noData), findsNothing);
+        });
       });
     });
 
