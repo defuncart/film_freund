ec4d4813b4deeacc515fd092c6c5a4783adc1760
James Leahy <defuncart@gmail.com>
2022-01-22 10:42:57 +0100

    Update loca.

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -8,6 +8,7 @@
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
 // ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -69,6 +70,8 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Add to list"),
         "movieTeaserBottomSheetShowMovieButtonText":
             MessageLookupByLibrary.simpleMessage("Show movie"),
+        "searchViewNoMoviesFoundText":
+            MessageLookupByLibrary.simpleMessage("No movies found"),
         "settingsViewChangePasswordButtonText":
             MessageLookupByLibrary.simpleMessage("Change Password"),
         "settingsViewDeleteAccountButtonText":
